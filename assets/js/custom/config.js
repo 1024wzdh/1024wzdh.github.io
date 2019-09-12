73cea95d2abbe350987cc8cbda95b70c09f23e21
1024wzdh <55244203+1024wzdh@users.noreply.github.com>
2019-09-12 21:57:25 +0800

    Add files via upload

---
+++ assets/js/custom/config.js
@@ -0,0 +1,7 @@
+window.line_1 = "https://1024dy1.com/";
+window.line_2 = "https://1024dy02.com/";
+window.line_3 = "https://1024dy1.ml/";
+window.line_4 = "https://1024dy1.tk/";
+window.line_5 = "https://1024dy1.cf/";
+window.line_6 = "https://1024dy1.ga/";
+window.default_line = "https://1024dy1.com/";
